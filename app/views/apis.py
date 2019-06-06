257a9a00c25de46d7fcf18befa9f89cb5eea1402
bitsnaps <bitsnaps@yahoo.fr>
2019-06-06 16:13:03 +0100

    adding GET example to api

--- app/views/apis.py
+++ app/views/apis.py
@@ -7,13 +7,19 @@ from flask_user import current_user, login_required, roles_accepted
 from app import db
 from app.models.user_models import UserProfileForm
 import uuid, json, os
-import datetime
+from datetime import datetime
 
 # When using a Flask app factory we must use a blueprint to avoid needing 'app' for '@app.route'
 api_blueprint = Blueprint('api', __name__, template_folder='templates')
 
+# curl "http://localhost:5000/timestamp"
+@api_blueprint.route('/timestamp')
+def timestamp():
+    ret = {"timestamp": datetime.utcnow()}
+    return(jsonify(ret), 200)
+
+# curl -X POST "http://localhost:5000/sample_call"
 @api_blueprint.route('/sample_call', methods=['POST'])
 def sample_page():
-
     ret = {"sample return": 10}
     return(jsonify(ret), 200)
